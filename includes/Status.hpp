5c48d9bb5bec49bf8e6697cb6b81e36df7f2166d
eunju-silver <yorkessw@gmail.com>
2021-07-07 20:28:43 +0900

    Add Status.hpp

---
+++ includes/Status.hpp
@@ -0,0 +1,10 @@
+#ifndef STATUS_HPP
+# define STATUS_HPP
+
+#define OK "200 OK"
+#define BADREQUEST "400 Bad Request"
+#define NOTFOUND "404 Not Found"
+#define INTERNALERROR "500 Internal Server Error"
+#define UNAVAILABLE "503 Service Unavailable"
+
+#endif
