6da6d292838a410e2e5d6cb03568c72443d65d95
rycando <rycando@naver.com>
2021-08-14 17:55:54 +0900

    fix

--- srcs/Config.cpp
+++ srcs/Config.cpp
@@ -90,7 +90,6 @@ void Config::parse_conf(std::string &buf, std::string line, std::string &context
         if (context != "")
             context += " ";
         context += tmp + "|";
-        std::cout << context << std::endl;
         getline(buf, line);
         while ((pos = line.find('}')) == std::string::npos)
         {
@@ -134,8 +133,6 @@ void Config::parse(char *file, std::vector<Server> &servers)
         std::vector<Server>::iterator it(servers.begin());
         while (it != servers.end())
         {
-            std::cout << config["server|"]["listen"] << std::endl;
-            std::cout << it->_conf.back()["server|"]["listen"] << std::endl;
             if (config["server|"]["listen"] == it->_conf.back()["server|"]["listen"])
             {
                 std::cout << config["server|"]["listen"] << " " << it->_conf.back()["server|"]["listen"] << std::endl;
